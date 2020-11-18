c83a743ef3cadfc3b078eecd5eceb816a039bb4c
rustami <1155102622@link.cuhk.edu.hk>
2020-11-18 17:10:30 +0800

    Small changes to build script

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-cd /var/www/biobuy
+cd /var/www/biobuy || exit
 git pull
 sudo npm install
-cd /var/www/biobuy/fintrack/frontend/
+cd /var/www/biobuy/fintrack/frontend/ || exit
 sudo npm install
 sudo npm run build
-cd /var/www/biobuy
+cd /var/www/biobuy || exit
 pm2 stop app
 pm2 start app.js
 sudo service nginx stop && sudo service nginx start
