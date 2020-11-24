c7123a86a08d9219380fc8d64d4e70a3686e71c0
rustami <1155102622@link.cuhk.edu.hk>
2020-11-24 19:57:28 +0800

    Mongo Setup

--- utilities/mongoConnect.js
+++ utilities/mongoConnect.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const config_param = fs.readFileSync(path.resolve(__dirname, "../config.json"), 'utf-8');
 const configJson = JSON.parse(config_param);
-const mongodbUrl = `mongodb+srv://${configJson.mongo.user}:${configJson.mongo.password}@cluster0-jfjzg.gcp.mongodb.net/test?retryWrites=true&w=majority`;
-
+const mongodbUrl = `mongodb+srv://${configJson.mongo.user}:${configJson.mongo.password}@cluster0.2g0d0.mongodb.net/biobuy?retryWrites=true&w=majority`;
 const connectMongo = () => new Promise(async (resolve, reject) => {
     if (configJson) {
         try {
